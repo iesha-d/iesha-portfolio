3a2855f0264b6d437ad2fddf9ed742420c4cb6ea
iesha-d <ieshad2006@gmail.com>
2023-11-02 19:23:03 -0400

    updated layout

--- pages/index.js
+++ pages/index.js
@@ -32,52 +32,50 @@ export default function Home() {
 
             <div className={styles.contentContainer}>
 
-                <h2>About Me</h2>
+                <h2 className={styles.topGap}>About Me</h2>
                 <p className={styles.about}>
-                    I'm an aspiring software engineer who's interested in AI and ML
-                    and loves taking apart complex systems.
-                    </p>
-                   <p className={styles.about}> 
-                    Glenelg High School Class of 2024, aiming for CS undergrad.
-                    
+                    I'm an aspiring software engineer who works at the intersection of ML, 
+                    formal systems, and art. I love taking apart complexity and turning noise into structure.
                 </p>
-                <h2>Projects & GitHub</h2>
+                <p className={styles.about}> 
+                    Glenelg High School Class of 2024, future CS undergrad.
+                </p>
+                <h2 className={styles.topGap}>Projects & GitHub</h2>
                 <PortfolioItem
                     title={"2048 Puzzle"}
                     image={'screenshots/2048.gif'}
-                    description={"A reversal of the classic puzzle game 2048. Here, the player must combine all the tiles into one."}
-                    tags={["MVC", "JavaScript/HTML/CSS"]}
+                    description={"A reversal of the classic puzzle game 2048. In this project, the player must combine all of tiles into one to win."}
+                    tags={["MVC", "Animation", "JavaScript/HTML/CSS"]}
                     link={"https://github.com/iesha-d/2048-puzzle"}
                 /> 
                 <PortfolioItem
                     title={"Minilisp"}
                     image={'screenshots/minilisp.png'}
-                    description={"Designed Minilisp, a simple Lisp-like language, and wrote a parser which represents it as a JS object."}
-                    tags={["Compilers", "Programming Languages"]}
+                    description={"Designed Minilisp, a simple Lisp-like language, and wrote a lexer and parser which represent it as an abstract syntax tree."}
+                    tags={["Compilers", "Programming Languages", "Parsing"]}
                     link={"https://github.com/iesha-d/minlisp"}
                     /> 
                 <PortfolioItem
                     title={"Virtual Machine"}
                     image={'screenshots/vm.gif'}
-                    description={"Built a VM to execute and compile commands in assembly."}
-                    tags={["Fetching from APIs", "Vanilla JS"]}
+                    description={"Built a VM for a simple physical CPU, along with a new instruction set, to learn about how instructions are executed and compiled by the hardware."}
+                    tags={["Assembly", "CPU", "Emulation", "Virtual Machines"]}
                     link={"https://github.com/iesha-d/virtual-machine"}
                 /> 
                 <PortfolioItem
                     title={"Mandelbrot Generator"}
                     image={'screenshots/mandel.png'}
-                    description={"Performance experiments in C++ for drawing a Mandelbrot."}
-                    tags={["C++", "PNG Manipulation", "Computer Graphics"]}
+                    description={"Single and Multithreaded performance experiments in C++ for drawing a Mandelbrot. Outputs results as PNG."}
+                    tags={["C++", "PNG Manipulation", "Computer Graphics", "Fractals"]}
                     link={"https://github.com/iesha-d/mandelbrot-generator"}
                 /> 
                 <PortfolioItem
                     title={"Weather Forecaster"}
                     image={'screenshots/weather.png'}
-                    description={"An easy-to-use React/JS app which gives the weekly weather forecast for any latitude and longitude coordinates and draws a graph based on the forecast."}
-                    tags={["Fetching from APIs", "Vanilla JS"]}
+                    description={"An easy-to-use React/JS app which gives the weekly weather forecast for any latitude and longitude and renders a bar chart of temperatures."}
+                    tags={["React", "UX", "APIs", "JavaScript"]}
                     link={"https://github.com/iesha-d/weather-forecaster"}
                 /> 
-                <h2>Hobbies / EC</h2>
             </div>
 
             <style jsx global>{`
